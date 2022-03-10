2897373c743cab7a9d20bb5a81cd932bbc05a5c8
unknown <alissonfabricio04@gmail.com>
2022-03-10 15:56:24 -0300

    add new controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express'
+import { AddDataController } from './controllers/AddDataController'
 import { SayHelloController } from './controllers/SayHelloController'
 
 
 const router = Router()
 
 const sayHelloController = new SayHelloController()
+const addDataController = new AddDataController()
 
 router.get('/', sayHelloController.handle)
-router.post('/add')
+router.post('/add', addDataController.handle)
 
 
 export { router }
